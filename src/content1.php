e12d3fb92f6d1293685f5a6f83db249f52bb13c3
watsokel <watsokel@onid.oregonstate.edu>
2015-04-30 00:41:40 -0700

    debugging login.php, figured out why username was not printing

--- src/content1.php
+++ src/content1.php
@@ -10,16 +10,16 @@ if(isset($_POST['submit'])){
 	if(empty($_POST['username'])){
 		echo 'A username must be entered. Click <a href="login.php">here</a> to return to the login screen.';
 		$_SESSION['loggedIn'] = false;
-	} //else{
-//		$_SESSION['loggedIn'] = true;		
-//		echo "Hello $_POST['userName']. ";
-//		echo 'Click <a href="content2.php">here</a> to go to content2.php.';
+	} else {
+		$_SESSION['loggedIn'] = true;		
+		echo "Hello $_POST[username].";
+//		echo ' Click <a href="content2.php">here</a> to go to content2.php.';
 //		if(!isset($_SESSION['visits'])){
 //			$_SESSION['visits'] = 0;
 //		}
 //		++$_SESSIONS['visits'];
 //		echo "Hi $_SESSION[user], you have visited this page $_SESSION['visits'] times."	
-//	}	
+	}	
 }
 else if(!isset($_POST['submit'])){
 	header('Location: https://web.engr.oregonstate.edu/~watsokel/cs290a4/login.php');
