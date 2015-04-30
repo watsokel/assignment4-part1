c1c0782d122e9c05ea0e7e9475a1029655091c76
watsokel <watsokel@onid.oregonstate.edu>
2015-04-30 01:16:12 -0700

    added logic to take into account already logged in users

--- src/content1.php
+++ src/content1.php
@@ -5,24 +5,37 @@ ini_set('display_errors',1);
 session_start();
 
 
-if(isset($_POST['submit'])){
-	$_SESSION['user'] = $_POST['username'];
-	if(empty($_POST['username'])){
+if(isset($_POST['submit']) && ($_SESSION['loggedIn']==false || !isset($_SESSION['loggedIn']))){
+	$_SESSION['name'] = $_POST['username'];
+	$_SESSION['loggedIn'] = true;
+	if(empty($_SESSION['name'])){
 		echo 'A username must be entered. Click <a href="login.php">here</a> to return to the login screen.';
 		$_SESSION['loggedIn'] = false;
 	} else {
 		$_SESSION['loggedIn'] = true;		
 		echo "Hello $_POST[username].";
-//		echo ' Click <a href="content2.php">here</a> to go to content2.php.';
-//		if(!isset($_SESSION['visits'])){
-//			$_SESSION['visits'] = 0;
-//		}
-//		++$_SESSIONS['visits'];
-//		echo "Hi $_SESSION[user], you have visited this page $_SESSION['visits'] times."	
+		echo ' Click <a href="content2.php">here</a> to go to content2.php.';
+		if(!isset($_SESSION['visits'])){
+			$_SESSION['visits'] = 0;
+		}
+		++$_SESSION['visits'];
+		echo "Hi $_SESSION[name], you have visited this page $_SESSION[visits] times.";	
 	}	
 }
-else if(!isset($_POST['submit'])){
+else if(!isset($_POST['submit']) && ($_SESSION['loggedIn']==false || !isset($_SESSION['loggedIn']))){
 	header('Location: https://web.engr.oregonstate.edu/~watsokel/cs290a4/login.php');
 	die();
 }
+
+
+if($_SESSION['loggedIn']){
+	++$_SESSION['visits'];
+	echo "Hi $_SESSION[name], you have visited this page $_SESSION[visits] times.";	
+}
+
+//if($_POST['username'] !== $_SESSION['name']){
+//	$_SESSION = array();
+//	session_destroy();
+//}
+
 ?>
